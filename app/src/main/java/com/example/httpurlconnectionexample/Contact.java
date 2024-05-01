bdeb8c97ab5e528b8eff1828275fa1909c05fc5a
dazizo25 <dazizo25@gmail.com>
2024-05-01 02:10:17 +0100

    created the Contact.java class that with all the filed need from the CPU company; to house contact list of information

---
+++ app/src/main/java/com/example/httpurlconnectionexample/Contact.java
@@ -0,0 +1,66 @@
+package com.example.httpurlconnectionexample;
+
+import androidx.annotation.NonNull;
+
+public class Contact {
+    @NonNull
+    String id;
+    String companyName;
+    String contactName;
+    String title;
+    String role;
+    String phoneNumber;
+    String extensionNumber;
+    String mobileNumber;
+    String emailAddress;
+    String  street;
+    String city;
+    String county;
+    String postCode;
+    String status;
+    String annualRevenue;
+
+
+    public Contact(@NonNull String id, String companyName, String contactName, String title,
+                   String role, String phoneNumber, String extensionNumber, String mobileNumber,
+                   String emailAddress, String street, String city, String county, String postCode,
+                   String status, String annualRevenue) {
+        this.id = id;
+        this.companyName = companyName;
+        this.contactName = contactName;
+        this.title = title;
+        this.role = role;
+        this.phoneNumber = phoneNumber;
+        this.extensionNumber = extensionNumber;
+        this.mobileNumber = mobileNumber;
+        this.emailAddress = emailAddress;
+        this.street = street;
+        this.city = city;
+        this.county = county;
+        this.postCode = postCode;
+        this.status = status;
+        this.annualRevenue = annualRevenue;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id='" + id + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", contactName='" + contactName + '\'' +
+                ", title='" + title + '\'' +
+                ", role='" + role + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", extensionNumber='" + extensionNumber + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", county='" + county + '\'' +
+                ", postCode='" + postCode + '\'' +
+                ", status='" + status + '\'' +
+                ", annualRevenue='" + annualRevenue + '\'' +
+                '}';
+    }
+
+}
